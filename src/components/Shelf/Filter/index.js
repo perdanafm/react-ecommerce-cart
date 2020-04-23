8614124d1828ac15668440c94050bacf900c67ee
Perdana Faishal Mulhaq <meetfaishal@gmail.com>
2020-04-23 21:03:21 +0700

    Initial Commit

---
+++ src/components/Shelf/Filter/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { updateFilters } from '../../../services/filters/actions';
+import Checkbox from '../../Checkbox';
+
+import './style.scss';
+
+const availableSizes = [
+  'Baru Minggu Ini',
+  'Menu Sehat',
+  'Promosi',
+  'Tervaforit'
+];
+
+class Filter extends Component {
+  static propTypes = {
+    updateFilters: PropTypes.func.isRequired,
+    filters: PropTypes.array
+  };
+
+  componentDidMount() {
+    this.selectedCheckboxes = new Set();
+  }
+
+  toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+
+    this.props.updateFilters(Array.from(this.selectedCheckboxes));
+  };
+
+  createCheckbox = label => (
+    <Checkbox
+      classes="filters-available-size"
+      label={label}
+      handleCheckboxChange={this.toggleCheckbox}
+      key={label}
+    />
+  );
+
+  createCheckboxes = () => availableSizes.map(this.createCheckbox);
+
+  render() {
+    return (
+      <div className="filters">
+        <h4 className="title">Pilih makananmu :</h4>
+        {this.createCheckboxes()}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { updateFilters }
+)(Filter);
---
+++ src/components/github/StarButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StarButton = () => (
+  <div className="star-button-container">
+    <h1> Good-Food </h1>
+    <h2> Memberikan Makanan yang terbaik</h2>
+  </div>
+);
+
+export default StarButton;
